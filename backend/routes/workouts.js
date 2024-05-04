e36f9e57963d0d70f68b8ebf5e2da9ba45bf0453
Jorge.Peniche <jorge.peniche@anahuac.mx>
2024-05-04 14:11:59 -0600

    test commit

--- backend/routes/workouts.js
+++ backend/routes/workouts.js
@@ -3,23 +3,23 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-    res.json({message: "GET all workouts"})
+    res.json({ message: "GET all workouts" })
 })
 
 router.get('/:id', (req, res) => {
-    res.json({message: "GET a single workout"})
+    res.json({ message: "GET a single workout" })
 })
 
 router.post('/', (req, res) => {
-    res.json({message: "POST a new workout"})
+    res.json({ message: "POST a new workout" })
 })
 
 router.delete('/:id', (req, res) => {
-    res.json({message: "DELETE a single workout"})
+    res.json({ message: "DELETE a single workout" })
 })
 
 router.patch('/:id', (req, res) => {
-    res.json({message: "UPDATE(PATCH) a single workout"})
-})
+    res.json({ message: "UPDATE(PATCH) a single workout" })
+});
 
 module.exports = router;
