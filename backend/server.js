2af574a6d128ea93f65ff7681c1319812b9d1ba9
Jorge Peniche <jorge.peniche@MacBookPro-JP.local>
2024-05-08 08:55:34 -0600

    React Front End initialized

--- backend/server.js
+++ backend/server.js
@@ -1,37 +1,37 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-require('dotenv').config();
+require("dotenv").config();
 
 //MODULES
-const workoutRoutes = require('./routes/workouts');
-
+const workoutRoutes = require("./routes/workouts");
 
 //CONFIG
-const port = process.env.PORT
+const port = process.env.PORT;
 
 //MIDDLEWARE
-app.use(express.json())
+app.use(express.json());
 app.use((req, res, next) => {
-    console.log(req.method+": "+req.path);
-    next();
-})
+  console.log(req.method + ": " + req.path);
+  next();
+});
 
-app.use('/api/workouts',workoutRoutes);
-
+app.use("/api/workouts", workoutRoutes);
 
 //Connect to db
-mongoose.connect(process.env.MONGO_URI)
-.then(()=>{
-
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
     //Start Server Listening
-    app.listen(port, () => console.log(`
+    app.listen(port, () =>
+      console.log(`
 Connected to database.
-Listening on port ${port}!`))
-})
-.catch((error)=>{
+Listening on port ${port}!`)
+    );
+  })
+  .catch((error) => {
     console.log(error);
-})
+  });
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.get("/", (req, res) => res.send("Hello World!"));
